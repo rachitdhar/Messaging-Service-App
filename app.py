0efe38179b4a2697b218639456b9a89f5f28c1f7
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2024-07-14 22:13:56 +0530

    fixed main bug

--- app.py
+++ app.py
@@ -8,24 +8,25 @@ import sys
 
 PORT = 8080
 EXIT_APP = False
+LOG_FILE_NAME = 'app_logs.txt'
+
 
 def handle_printing_received_message(sent_by, received_msg):
-    curr_line = sys.stdin.readline()
-    is_curr_line_blank = True if (len(curr_line.strip()) == 0) else False
-    
-    # move the text from the current line below the received message
-    if not is_curr_line_blank:
-        sys.stdout.write("\r")
-        sys.stdout.write("\033[K")
-        sys.stdout.flush()
-    
-    print(Fore.GREEN + f"~{sent_by} : " + Style.RESET_ALL + received_msg)
-
-    if not is_curr_line_blank:
-        print(curr_line, end='')
+    sys.stdout.write("\0337")           # Save the current cursor position
+    sys.stdout.write("\033[0G")         # Move to the beginning of the line
+    sys.stdout.write(Fore.GREEN + f"~{sent_by} : " + Style.RESET_ALL + received_msg + "\n")
+    sys.stdout.write("\0338")           # Restore the cursor position
+    sys.stdout.write("\033[B")          # Move the cursor down one line
+    sys.stdout.flush()
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    # overriding this method to redirect the logs to the log file
+    def log_message(self, format, *args):
+        with open(LOG_FILE_NAME, 'a') as flogs:
+            flogs.write(f"{self.address_string()} - - [{self.log_date_time_string()}] {format % args}")
+            flogs.write("\n\n")
+
     def do_POST(self):
         # Determine the length of the incoming data
         content_length = int(self.headers['Content-Length'])
@@ -36,12 +37,12 @@ class RequestHandler(BaseHTTPRequestHandler):
         try:
             data = json.loads(post_data)
             handle_printing_received_message(data['sentBy'], data['msg'])
-
+            
             # Send a response
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-
+            
             message = "CONNECTION SUCCESSFUL" if (data['sentBy'] == 'APP_CONN_CHECK') else "POST request was successful"
             response = json.dumps({'status': 'SUCCESS', 'message': message})
             self.wfile.write(response.encode('utf-8'))
@@ -88,9 +89,10 @@ def sender(username, ipaddr):
 
         try:
             response = requests.post(url, json=payload, headers={"Content-Type": "application/json"})
-            log_data = (f'Receiver ipaddress: {ipaddr}\nStatus: {response.status}\nMessage: {response.message}')
+            response = json.loads(response.text)
+            log_data = (f"Receiver ipaddress: {ipaddr}\nStatus: {response['status']}\nMessage: {response['message']}")
             
-            with open('app_logs.txt', 'a') as flogs:
+            with open(LOG_FILE_NAME, 'a') as flogs:
                 flogs.write(log_data)
                 flogs.write("\n\n")
         except requests.exceptions.RequestException as e:
@@ -101,7 +103,6 @@ def receiver():
     hostname = socket.gethostname()
     server_address = (socket.gethostbyname(hostname), PORT)
     httpd = HTTPServer(server_address, RequestHandler)
-    #print(Fore.GREEN + f'Starting httpd server on PORT {PORT}' + Style.RESET_ALL)
     httpd.serve_forever()
 
 
@@ -129,48 +130,53 @@ def main():
 
     while (not EXIT_APP):
         ipaddr = input(Fore.CYAN + "Enter ipaddress of the user you want to send messages to: " + Style.RESET_ALL)
-
+        
         # checking if ipaddress is valid
         url = f"http://{ipaddr}:{PORT}/get-msg"
         
         try:
-            response = requests.post(url, json={'sentBy':'APP_CONN_CHECK','msg':f'Testing connection.'}, headers={"Content-Type": "application/json"})
-            log_data = (f'Receiver ipaddress: {ipaddr}\nStatus: {response.status}\nMessage: {response.message}')
+            response = requests.post(url, json={'sentBy':'APP_CONN_CHECK','msg':'Testing connection.'}, headers={"Content-Type": "application/json"})
+            response = json.loads(response.text)
+            log_data = (f"Receiver ipaddress: {ipaddr}\nStatus: {response['status']}\nMessage: {response['message']}")
             
-            if response.status == "CONNECTION SUCCESSFUL":
+            if response['status'] == "CONNECTION SUCCESSFUL":
                 print(Fore.GREEN + "Connection Successful." + Style.RESET_ALL)
             else:
                 print(Fore.YELLOW + "Connected, but did not receive confirmatory response." + Style.RESET_ALL)
 
             print()
 
-            with open('app_logs.txt', 'a') as flogs:
+            with open(LOG_FILE_NAME, 'a') as flogs:
                 flogs.write(log_data)
                 flogs.write("\n\n")
         except socket.gaierror:
             print(Fore.RED + "IP address could not be resolved. Try again." + Style.RESET_ALL)
 
-            with open('app_logs.txt', 'a') as flogs:
+            with open(LOG_FILE_NAME, 'a') as flogs:
                 flogs.write(f"Receiver ipaddress: {ipaddr}\nIP address could not be resolved.")
                 flogs.write("\n\n")
             continue
         except Exception:
             print(Fore.RED + "An error occurred while trying to connect. Try again." + Style.RESET_ALL)
             
-            with open('app_logs.txt', 'a') as flogs:
+            with open(LOG_FILE_NAME, 'a') as flogs:
                 flogs.write(f"Receiver ipaddress: {ipaddr}\nAn error occurred while trying to connect.")
                 flogs.write("\n\n")
             continue
-
+        
         sender_thread = th.Thread(target=sender, args=(username, ipaddr))
         sender_thread.start()
 
-        # wait for sender thread to complete
+        #wait for sender thread to complete
         sender_thread.join()
         print()
 
     # wait for receiver thread to complete
     receiver_thread.join()
+
+    with open(LOG_FILE_NAME, 'a') as flogs:
+        flogs.write("------------ SESSION OVER ------------")
+        flogs.write("\n\n")
 
     print(Fore.CYAN + "Message Service has been exited. Press any key to close this window..." + Style.RESET_ALL)
     input()
